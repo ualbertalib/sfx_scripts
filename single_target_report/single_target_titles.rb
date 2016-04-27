625fc28690a623d1fa338388abff55c488b2b0ab
redlibrarian <spopowich@gmail.com>
2016-04-27 09:42:27 -0600

    Update single_target_titles.rb

--- single_target_report/single_target_titles.rb
+++ single_target_report/single_target_titles.rb
@@ -1,31 +1,37 @@
 require "marc"
 
+# Class to represent a list of JOURNALS which have TARGETS
 class SingleTargetTitles
 
   attr_reader :all, :matches
 
+  # Instantiate the class
   def initialize(datafile)
     begin
-      data = MARC::XMLReader.new(File.open(datafile))
+      data = MARC::XMLReader.new(File.open(datafile))   # Read a MARCXML data file
     rescue => e
       puts "Unable to open file. Exiting (#{e})"
       exit
     end
-    @all = data.entries
+    @all = data.entries    # Create a list of journals from the data file
   end
 
+  # Returns how many journals had a given target name
   def matches(query)
     @matches ||= search(query)
   end
 
+  # Returns the number of targets for a given journal
   def number_of_targets(index)
     @all[index].to_hash["fields"].select{|field| field.keys.first == "866" }.size
   end
 
+  # Does the journal only have one target?
   def single?(index)
     number_of_targets(index) == 1
   end
 
+  # Return a list of single-target journals that match the given target name
   def for(query)
     single_target_titles = []
     matches(query).each do |match|
@@ -34,6 +40,7 @@ class SingleTargetTitles
     single_target_titles
   end
 
+  # Convert the list of single-target journals to CSV
   def csv(query)
     titles_array = self.for(query)
     titles_array.each_with_object([]) do |title, csv_array|
@@ -43,6 +50,7 @@ class SingleTargetTitles
 
   private
 
+  # Search through each journal for a given target name
   def search(query)
     @all.each_with_object([]) do |record, matches|
       matches << record if ((record['866']['t'] == query) || (record['866']['s'] == query))
